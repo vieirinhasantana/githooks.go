63e5676f298f1bb187559e00c9738a73eb506a25
sant1 <vieirinhasantana@gmail.com>
2021-05-28 23:49:33 -0300

    Add files via upload

---
+++ pre-commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "==> Checking that code complies with gofmt requirements..."
+gofmt_files=$(gofmt -l `find . -name '*.go' | grep -v vendor`)
+if [[ -n ${gofmt_files} ]]; then
+    echo 'gofmt needs running on the following files:'
+    echo "${gofmt_files}"
+    echo "You can use the command: \`gofmt -w \$(gofmt -l \`find . -name \'*.go\' | grep -v vendor)\` to reformat code."
+    exit 1
+fi
+
+git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
+
+GO111MODULE=on go get -u -t github.com/golangci/golangci-lint/cmd/golangci-lint
+golangci-lint run --tests=false --skip-files=mock.go --disable=goimports --enable-all
+
+exit $?
